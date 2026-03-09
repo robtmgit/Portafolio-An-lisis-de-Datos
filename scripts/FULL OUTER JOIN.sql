e74f4706bdaa7d64c92dd915169568af59037478
robtmgit <166878136+robtmgit@users.noreply.github.com>
2026-03-08 21:03:04 -0600

    Create FULL OUTER JOIN.sql

---
+++ scripts/FULL OUTER JOIN.sql
@@ -0,0 +1,9 @@
+select
+employees.name AS employee_name,
+employees.role AS employee_role,
+departments.name AS department_name
+FROM
+employee_data.employees
+FULL OUTER JOIN
+employee_data.departments ON
+employees.department_id = departments.department_id
